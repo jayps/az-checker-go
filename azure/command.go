7d98432116440c114d17f344a771f93f7059a235
JP Meyer <jp@jpmeyer.dev>
2023-01-04 14:17:47 +0200

    Updated command execution to be platform specific

--- azure/command.go
+++ azure/command.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"runtime"
 )
 
+func Execute(command string) ([]byte, error) {
+	if runtime.GOOS == "windows" {
+		return exec.Command("powershell", command).Output()
+	}
+
+	return exec.Command("bash", "-c", command).Output()
+}
+
 func RunCommand(command string) []byte {
-	output, err := exec.Command("powershell", command).Output()
+	output, err := Execute(command)
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Command '%s' failed with error %s", command, err.Error()))
 	}
--- azure/subscription.go
+++ azure/subscription.go
@@ -3,11 +3,10 @@ package azure
 import (
 	"fmt"
 	"log"
-	"os/exec"
 )
 
 func SetSubscription(subscriptionId string) {
-	_, err := exec.Command("powershell", fmt.Sprintf("az account set --subscription %s", subscriptionId)).Output()
+	_, err := Execute(fmt.Sprintf("az account set --subscription %s", subscriptionId))
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Could not set subscription ID: %s", err.Error()))
 	}
