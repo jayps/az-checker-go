6af857e5419b4d2801e47e4454fbd36e2a6c6c85
JP Meyer <jp@jpmeyer.dev>
2023-01-10 12:24:37 +0200

    Concurrently fetch backups

--- azure/backups.go
+++ azure/backups.go
@@ -2,43 +2,69 @@ package azure
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"sync"
 )
 
-func FetchBackupsForVM(vmId string) (*Resource, error) {
-	// The vault ID will be returned
+type VMBackupResult struct {
+	Vault            *Resource
+	VirtualMachineId string
+	err              error
+}
+
+func FetchBackupsForVM(vmId string, backups chan<- VMBackupResult, wg *sync.WaitGroup) {
 	fmt.Println(fmt.Sprintf("Checking backups for VM %s...", vmId))
 	output, err := RunCommand(fmt.Sprintf("az backup protection check-vm --vm %s", vmId))
 	backupVaultId := string(output)
 
 	if err != nil {
-		return nil, err
+		fmt.Println(err.Error())
+		backups <- VMBackupResult{nil, vmId, err}
+		wg.Done()
+		return
 	}
 
 	if backupVaultId != "" {
 		output, err := FetchResourceDetails(backupVaultId)
 
 		if err != nil {
-			return nil, err
+			backups <- VMBackupResult{nil, vmId, err}
 		}
 
 		var vault Resource
 		err = json.Unmarshal(output, &vault)
-
-		return &vault, err
+		if err == nil {
+			fmt.Println(fmt.Sprintf("Found backup vault for %s.", vmId))
+		}
+		backups <- VMBackupResult{&vault, vmId, err}
+	} else {
+		fmt.Println(fmt.Sprintf("No backup vault for %s.", vmId))
+		backups <- VMBackupResult{nil, vmId, errors.New(fmt.Sprintf("No backup vault provided for VM %s", vmId))}
 	}
-
-	return nil, nil
+	wg.Done()
 }
 
 func FetchVMBackups(vms map[string]Resource) error {
-	for id, vm := range vms {
-		backupVault, err := FetchBackupsForVM(id)
-		if err != nil {
-			return err
+	backups := make(chan VMBackupResult, len(vms))
+	var wg sync.WaitGroup
+
+	for id := range vms {
+		wg.Add(1)
+		go FetchBackupsForVM(id, backups, &wg)
+	}
+
+	go func() {
+		defer close(backups)
+		wg.Wait()
+	}()
+
+	for b := range backups {
+		if b.err == nil {
+			vm := vms[b.VirtualMachineId]
+			vm.BackupVault = b.Vault
+			vms[b.VirtualMachineId] = vm
 		}
-		vm.BackupVault = backupVault
-		vms[id] = vm
 	}
 
 	return nil
